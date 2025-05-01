0a01f065d86bbc5d066e703f06b57f278e888771
sugasaki <sugasaki@gmail.com>
2025-05-01 22:11:02 +0900

    add MarkerTypes

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { StyleSheet, View } from 'react-native';
 import MapComponent from '@/components/MapComponent';
+import MarkerTypes from '@/components/MarkerTypes';
 
 export default function MapScreen() {
   return (
     <View style={styles.container}>
-      <MapComponent />
+      {/* <MapComponent /> */}
+      <MarkerTypes />/
     </View>
   );
 }
