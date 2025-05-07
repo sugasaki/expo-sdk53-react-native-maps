0fe7bceb417548e4bcb7489d6a0174ffba1632ea
sugasaki <sugasaki@gmail.com>
2025-05-07 22:39:03 +0900

    Add custom markers and callouts components with interactive map features

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,12 +3,17 @@ import { StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import MapComponent from '@/components/MapComponent';
 import MarkerTypes from '@/components/MarkerTypes';
+import CustomMarkers from '@/components/CustomMarkers';
+import Callouts from '@/components/Callouts';
 
 export default function MapScreen() {
   return (
     <ThemedView style={styles.container}>
       {/* <MapComponent /> */}
       <MarkerTypes />
+      {/* <MarkerTypes /> */}
+      {/* <CustomMarkers /> */}
+      <Callouts />
     </ThemedView>
   );
 }
